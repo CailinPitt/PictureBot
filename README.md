a6020317244924fb2b112b64d887004a23b8f6fe
Cailin Pitt <cailinpitt1@gmail.com>
2016-08-30 10:26:42 -0400

    Randomly retrieves & classifies image from 1 of 15 categories

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PictureBot
-Twitter bot which uses IBM Watson to guess what is in an image
+Twitter bot which uses IBM Watson Developer Cloud to guess what is in an image
+
+## npm Packages
+* faker
+* request
+* fs
+* watson-developer-cloud
--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ classifyImage();
 
 function classifyImage() {
 
-  var imageUrl = faker.Image.imageUrl();
+  var imageUrl = chooseImage();
   // Get image URL to classify
 
   var stream = request(imageUrl).pipe(fs.createWriteStream('image.png'));
@@ -33,4 +33,43 @@ function classifyImage() {
         console.log(JSON.stringify(res, null, 2));
     });
   });
+}
+
+function chooseImage() {
+  var imageUrl;
+  var randomNumber = faker.random.number(14);
+  // Get random number between 1 - 14
+
+  if (randomNumber == 0)
+    imageUrl = faker.Image.avatar();
+  else if (randomNumber == 1)
+    imageUrl = faker.Image.imageUrl();
+  else if (randomNumber == 2)
+    imageUrl = faker.Image.abstractImage();
+  else if (randomNumber == 3)
+    imageUrl = faker.Image.animals();
+  else if (randomNumber == 4)
+    imageUrl = faker.Image.business();
+  else if (randomNumber == 5)
+    imageUrl = faker.Image.cats();
+  else if (randomNumber == 6)
+    imageUrl = faker.Image.city();
+  else if (randomNumber == 7)
+    imageUrl = faker.Image.food();
+  else if (randomNumber == 8)
+    imageUrl = faker.Image.nightlife();
+  else if (randomNumber == 9)
+    imageUrl = faker.Image.fashion();
+  else if (randomNumber == 10)
+    imageUrl = faker.Image.people();
+  else if (randomNumber == 11)
+    imageUrl = faker.Image.nature();
+  else if (randomNumber == 12)
+    imageUrl = faker.Image.sports();
+  else if (randomNumber == 13)
+    imageUrl = faker.Image.technics();
+  else
+    imageUrl = faker.Image.transport();
+
+  return imageUrl;
 }
