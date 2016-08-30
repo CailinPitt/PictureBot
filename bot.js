46fb3488cce007522b96d921b5fe6099b464da0d
Cailin Pitt <cailinpitt1@gmail.com>
2016-08-30 09:44:26 -0400

    Download image, use Watson service to classify

--- bot.js
+++ bot.js
@@ -1,12 +1,36 @@
 var faker = require('Faker');
 var request = require('request');
 var fs = require('fs');
+var watson = require('watson-developer-cloud');
 
-// Download random image to detect
-downloadImage();
+// Download random image to classify
+classifyImage();
 
-function downloadImage() {
+function classifyImage() {
+
   var imageUrl = faker.Image.imageUrl();
+  // Get image URL to classify
 
-  request(imageUrl).pipe(fs.createWriteStream('image.png'));
+  var stream = request(imageUrl).pipe(fs.createWriteStream('image.png'));
+  // Download image
+
+  stream.on('finish', function() {
+    // When image is done downloading, classify
+    var visual_recognition = watson.visual_recognition({
+      api_key: process.env.WATSON_API,
+      version: 'v3',
+      version_date: '2016-05-20'
+    });
+
+    var params = {
+      images_file: fs.createReadStream('./image.png')
+    };
+
+    visual_recognition.classify(params, function(err, res) {
+      if (err)
+        console.log(err);
+      else
+        console.log(JSON.stringify(res, null, 2));
+    });
+  });
 }
